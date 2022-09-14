246b2415a1d3c00cb39c20ffca2d718d2554082a
Shaswat Misra <shaswatmisra2310@gmail.com>
2022-09-14 21:37:39 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿var client = new HttpClient();
+var request = new HttpRequestMessage
+{
+    Method = HttpMethod.Get,
+    RequestUri = new Uri("https://yh-finance.p.rapidapi.com/market/v2/get-quotes?region=US&symbols=AMD%2CIBM%2CAAPL"),
+    Headers =
+    {
+        { "X-RapidAPI-Key", "ed8bbc525fmshe7f2ca46e69c3ffp179016jsne9ad147cd3e0" },
+        { "X-RapidAPI-Host", "yh-finance.p.rapidapi.com" },
+    },
+};
+
+using (var response = await client.SendAsync(request))
+{
+    response.EnsureSuccessStatusCode();
+    var body = await response.Content.ReadAsStringAsync();
+    var body1 = response.Content.Headers;
+    Console.WriteLine(body);
+    Console.WriteLine(body1);
+    File.WriteAllText(@"C:\Users\shasw\Desktop\cron.txt", body);
+}
