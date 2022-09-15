c4240b7d132b467095ded9c5cf66e494f8fa5339
Shaswat Misra <shaswatmisra2310@gmail.com>
2022-09-15 15:59:01 +0530

    to do

--- Program.cs
+++ Program.cs
@@ -4,11 +4,11 @@ var client = new HttpClient();
 var request = new HttpRequestMessage
 {
     Method = HttpMethod.Get,
-    RequestUri = new Uri("https://yh-finance.p.rapidapi.com/market/v2/get-quotes?region=US&symbols=AMD%2CIBM%2CAAPL"),
+    RequestUri = new Uri("https://yh-finance.p.rapidapi.com/market/v2/get-quotes?region=US&symbols=AMD%2CIBM%2CAAPL"),  //from db
     Headers =
     {
-        { "X-RapidAPI-Key", "ed8bbc525fmshe7f2ca46e69c3ffp179016jsne9ad147cd3e0" },
-        { "X-RapidAPI-Host", "yh-finance.p.rapidapi.com" },
+        { "X-RapidAPI-Key", "ed8bbc525fmshe7f2ca46e69c3ffp179016jsne9ad147cd3e0" },     //from db
+        { "X-RapidAPI-Host", "yh-finance.p.rapidapi.com" },                             //from db
     },
 };
 
@@ -23,12 +23,12 @@ using (var response = await client.SendAsync(request))
 }
 
 
-JObject data = JObject.Parse(File.ReadAllText("C:\Users\shasw\Desktop\cron.json"));
+JObject data = JObject.Parse(File.ReadAllText("C:\Users\shasw\Desktop\cron.json"));         //to db
 
-var language = data.SelectToken("quoteResponse.result[0].language")?.Value<string>();
-var quoteType = data.SelectToken("quoteResponse.result[0].quoteType")?.Value<string>();
-var TotalCash = data.SelectToken("quoteResponse.result[0].totalCash")?.Value<string>();
-var floatShares = data.SelectToken("quoteResponse.result[0].floatShares")?.Value<string>();
-var preMarketChangePercent = data.SelectToken("quoteResponse.result[0].preMarketChangePercent")?.Value<string>();
-var targetPriceHigh = data.SelectToken("quoteResponse.result[0].targetPriceHigh")?.Value<string>();
+var language = data.SelectToken("quoteResponse.result[0].language")?.Value<string>();          //to db
+var quoteType = data.SelectToken("quoteResponse.result[0].quoteType")?.Value<string>();         //to db
+var TotalCash = data.SelectToken("quoteResponse.result[0].totalCash")?.Value<string>();        //to db
+var floatShares = data.SelectToken("quoteResponse.result[0].floatShares")?.Value<string>();    //to db
+var preMarketChangePercent = data.SelectToken("quoteResponse.result[0].preMarketChangePercent")?.Value<string>();        //to db
+var targetPriceHigh = data.SelectToken("quoteResponse.result[0].targetPriceHigh")?.Value<string>();        //to db
 
